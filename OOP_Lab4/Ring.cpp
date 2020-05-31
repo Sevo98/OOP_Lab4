80d10acbe493c095248ede8f9a14ed813168cb06
Sevo98 <sevo98.sk@gmail.com>
2020-05-31 19:22:41 +0600

    4'th laboratory work complete

--- OOP_Lab4/Ring.cpp
+++ OOP_Lab4/Ring.cpp
@@ -103,44 +103,44 @@ Ring::~Ring()
 	Ring::AllRingsCount--;
 	cout << "Осталось: " << Ring::AllRingsCount << endl;
 }
-
-void DemoRing()
-{
-
-	cout << "Количество колец до вызова конструктора: " << Ring::GetAllRingsCount() << endl;
-
-	Ring* ring = new Ring(Point(25.0, 25.0), 10.0, 5.0);
-	cout << "Количество колец после вызова конструктора: " << Ring::GetAllRingsCount() << endl;
-
-	delete ring;
-	cout << "Количество колец после вызова деструктора: " << Ring::GetAllRingsCount() << endl;
-
-	const int countRings = 3;
-	Ring rings[countRings];
-
-	for (int i = 0; i < countRings; i++)
-	{
-		double innerRadius, outerRadius;
-		cout << "Введите внутренний радиус "<< i+1 <<"-го кольца: ";
-		CheckInput::CheckInputDouble(&innerRadius);
-		cout << "\nВведите внешний радиус  " << i + 1 << "-го кольца:";
-		CheckInput::CheckInputDouble(&outerRadius);
-
-		Point point;
-		cout << "\nВведите центр  " << i + 1 << "-го кольца: " << endl;
-		double X, Y;
-		cout << "Введите X: ";
-		CheckInput::CheckInputDouble(&X);
-		cout << "\nВведите Y: ";
-		CheckInput::CheckInputDouble(&Y);
-
-		point = Point(X, Y);
-		
-		rings[i] = Ring(point, outerRadius, innerRadius);
-		cout << "Создан: " << Ring::GetAllRingsCount() << endl;
-		rings[i].WriteRingFromConsole();
-		double area;
-		area = rings[i].GetArea();
-		cout << "Площадь  " << i + 1 << "-го кольца: " << area << endl;
-	}
-}
+//
+//void DemoRing()
+//{
+//
+//	cout << "Количество колец до вызова конструктора: " << Ring::GetAllRingsCount() << endl;
+//
+//	Ring* ring = new Ring(Point(25.0, 25.0), 10.0, 5.0);
+//	cout << "Количество колец после вызова конструктора: " << Ring::GetAllRingsCount() << endl;
+//
+//	delete ring;
+//	cout << "Количество колец после вызова деструктора: " << Ring::GetAllRingsCount() << endl;
+//
+//	const int countRings = 3;
+//	Ring rings[countRings];
+//
+//	for (int i = 0; i < countRings; i++)
+//	{
+//		double innerRadius, outerRadius;
+//		cout << "Введите внутренний радиус "<< i+1 <<"-го кольца: ";
+//		CheckInput::CheckInputDouble(&innerRadius);
+//		cout << "\nВведите внешний радиус  " << i + 1 << "-го кольца:";
+//		CheckInput::CheckInputDouble(&outerRadius);
+//
+//		Point point;
+//		cout << "\nВведите центр  " << i + 1 << "-го кольца: " << endl;
+//		double X, Y;
+//		cout << "Введите X: ";
+//		CheckInput::CheckInputDouble(&X);
+//		cout << "\nВведите Y: ";
+//		CheckInput::CheckInputDouble(&Y);
+//
+//		point = Point(X, Y);
+//		
+//		rings[i] = Ring(point, outerRadius, innerRadius);
+//		cout << "Создан: " << Ring::GetAllRingsCount() << endl;
+//		rings[i].WriteRingFromConsole();
+//		double area;
+//		area = rings[i].GetArea();
+//		cout << "Площадь  " << i + 1 << "-го кольца: " << area << endl;
+//	}
+//}
--- OOP_Lab4/Ring.h
+++ OOP_Lab4/Ring.h
@@ -15,12 +15,9 @@ public:
 	double GetOuterRadius();
 	double GetPointX();
 	double GetPointY();
+	double GetArea();
 
 	Ring();
 	Ring(Point center, double outerRadius, double innerRadius);
 	~Ring();
-
-	double GetArea();
 };
-
-void DemoRing();
